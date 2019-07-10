bb84c5a9354c2de5ae8e39898e85912da3f94548
Javier Lopez Lopez <sjavierlopez@gmail.com>
2019-07-09 23:05:34 -0500

    more refactor

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -10,15 +10,36 @@ func TestHello(t *testing.T) {
 		}
 	}
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Javier")
+	/*
+		t.Run("saying hello to people", func(t *testing.T) {
+			got := Hello("Javier")
+			want := "Hello, Javier"
+			assertCorrectMessage(t, got, want)
+		})
+
+		t.Run("saying hello to people", func(t *testing.T) {
+			got := Hello("")
+			want := "Hello, World"
+			assertCorrectMessage(t, got, want)
+		})
+	*/
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Javier", "Spanish")
+		want := "Hola, Javier"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in English", func(t *testing.T) {
+		got := Hello("Javier", "English")
 		want := "Hello, Javier"
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Javier", "French")
+		want := "Bonjour, Javier"
 		assertCorrectMessage(t, got, want)
 	})
+
 }
